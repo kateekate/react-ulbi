80fe72aac7692275f153f1509458588959dd1008
Katya <katya.mikhaylova.98@list.ru>
2023-01-10 22:39:19 +0300

    two different lists w/ different content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import ClassCounter from "./Components/ClassCounter";
+import Counter from "./Components/Counter";
+import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
+import './styles/App.css'
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JS', body: 'Description 1' },
+    { id: 2, title: 'React', body: 'Description 2' },
+    { id: 3, title: 'Redux', body: 'Description 3' },
+  ])
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description 1' },
+    { id: 2, title: 'Python', body: 'Description 2' },
+    { id: 3, title: 'Python', body: 'Description 3' },
+  ])
+
+
   return (
     <div className="App">
-
+      <PostList posts={posts} title='Posts about JS ' />
+      <PostList posts={posts2} title='Posts about Python' />
     </div>
   );
 }
---
+++ src/Components/PostItem.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const PostItem = (props) => {
+  return (
+    <div div className="post" >
+      <div className="post__content">
+        <strong>{props.post.id}. {props.post.title}</strong>
+        <div>
+          {props.post.body}
+        </div>
+      </div>
+      <div className="post__btn">
+        <button>Delete</button>
+      </div>
+    </div >
+  )
+}
+
+export default PostItem
